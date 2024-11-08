fade28b0da83f22f612e24a70118e3529d7e42ac
Serge Ratke <serge.ratke@gmail.com>
2024-11-08 06:27:40 +0100

    namen und schalter einlesen.

--- b/prog.c
+++ b/prog.c
@@ -4,5 +4,7 @@ int main(int argc, char *argv[])
 {
     otf_schrift_t *fira_code_schrift = otf_schrift_lesen("FiraCode-Regular.ttf");
 
+    printf("%s: %s\n", fira_code_schrift->artname, fira_code_schrift->stilname);
+
     return 0;
 }
--- q/glyphe.h
+++ q/glyphe.h
@@ -36,11 +36,11 @@ typedef struct {
     int16_t x;
     int16_t y;
     uint8_t in_kontur;
-} otf_glyphe_punkt_t;
+} otf_punkt_t;
 
 // otf_glyphe_t Kontur
 typedef struct {
-    otf_glyphe_punkt_t* punkte;
+    otf_punkt_t* punkte;
     int punkte_anzahl;
 } otf_kontur_t;
 
@@ -68,6 +68,24 @@ typedef struct {
     int16_t unterlänge;
     int16_t zeilenabstand;
 } otf_schrift_t;
+
+// Neue Strukturen für die Name-Tabelle
+typedef struct {
+    uint16_t platform_id;
+    uint16_t encoding_id;
+    uint16_t language_id;
+    uint16_t name_id;
+    uint16_t länge;
+    uint16_t versatz;
+    char* string;  // UTF-16BE kodierter String
+} otf_name_eintrag_t;
+
+typedef struct {
+    uint16_t format;
+    uint16_t anzahl;
+    uint16_t string_versatz;
+    otf_name_eintrag_t* einträge;
+} otf_name_tabelle_t;
 
 // Hilfsfunktion zum Lesen von Big-Endian Werten
 uint32_t
@@ -100,6 +118,166 @@ otf_s16_lesen(FILE* datei)
     // Konvertierung von Big-Endian nach Host-Endian
     return ((wert & 0xFF00) >> 8) |
            ((wert & 0x00FF) << 8);
+}
+
+// Funktion zum Konvertieren von UTF-16BE zu UTF-8
+char *
+utf16be_zu_utf8(const uint8_t* utf16be, size_t länge) 
+{
+    if (länge % 2 != 0) return NULL;
+
+    // Maximal 4 Bytes pro UTF-16 Character in UTF-8
+    char* utf8 = (char*)GLYPHE_RES(länge * 2 + 1);
+    size_t utf8_pos = 0;
+
+    for (size_t i = 0; i < länge; i += 2)
+    {
+        uint16_t codepoint = (utf16be[i] << 8) | utf16be[i + 1];
+
+        if (codepoint < 0x80) {
+            utf8[utf8_pos++] = codepoint;
+        } else if (codepoint < 0x800) {
+            utf8[utf8_pos++] = 0xC0 | (codepoint >> 6);
+            utf8[utf8_pos++] = 0x80 | (codepoint & 0x3F);
+        } else {
+            utf8[utf8_pos++] = 0xE0 | (codepoint >> 12);
+            utf8[utf8_pos++] = 0x80 | ((codepoint >> 6) & 0x3F);
+            utf8[utf8_pos++] = 0x80 | (codepoint & 0x3F);
+        }
+    }
+    utf8[utf8_pos] = '\0';
+    
+    return utf8;
+}
+
+// Funktion zum Einlesen der Name-Tabelle
+void
+otf_name_tabelle_lesen(FILE* datei, otf_schrift_t* schrift, otf_tabelle_t* name_tabelle)
+{
+    if (!name_tabelle) return;
+
+    fseek(datei, name_tabelle->versatz, SEEK_SET);
+
+    // Format und Anzahl der Name Records
+    uint16_t format = otf_u16_lesen(datei);
+    uint16_t anzahl = otf_u16_lesen(datei);
+    uint16_t string_versatz = otf_u16_lesen(datei);
+
+    // Position des String-Storage (relativ zum Tabellenanfang)
+    long string_basis = name_tabelle->versatz + string_versatz;
+
+    // Temporäres Array für Name Records
+    otf_name_eintrag_t* einträge = (otf_name_eintrag_t*)
+        GLYPHE_RES(sizeof(otf_name_eintrag_t) * anzahl);
+
+    // Name Records einlesen
+    for (int i = 0; i < anzahl; i++)
+    {
+        einträge[i].platform_id = otf_u16_lesen(datei);
+        einträge[i].encoding_id = otf_u16_lesen(datei);
+        einträge[i].language_id = otf_u16_lesen(datei);
+        einträge[i].name_id = otf_u16_lesen(datei);
+        einträge[i].länge = otf_u16_lesen(datei);
+        einträge[i].versatz = otf_u16_lesen(datei);
+
+        // Aktuelle Position merken
+        long current_pos = ftell(datei);
+
+        // Zum String springen
+        fseek(datei, string_basis + einträge[i].versatz, SEEK_SET);
+
+        // String einlesen
+        uint8_t* raw_string = (uint8_t*)GLYPHE_RES(einträge[i].länge);
+        fread(raw_string, 1, einträge[i].länge, datei);
+
+        // Konvertierung zu UTF-8 wenn nötig
+        if (einträge[i].platform_id == 0 || // Unicode
+            (einträge[i].platform_id == 3 && einträge[i].encoding_id == 1))
+        {
+            // Windows Unicode
+            einträge[i].string = utf16be_zu_utf8(raw_string, einträge[i].länge);
+        }
+        else
+        {
+            // Für andere Encodings einfach als ASCII behandeln
+            einträge[i].string = (char*)GLYPHE_RES(einträge[i].länge + 1);
+            memcpy(einträge[i].string, raw_string, einträge[i].länge);
+            einträge[i].string[einträge[i].länge] = '\0';
+        }
+
+        GLYPHE_FRG(raw_string);
+
+        // Relevante Strings in der Schrift-Struktur speichern
+        if ((einträge[i].platform_id == 3 && einträge[i].language_id == 0x0409))
+        {
+            // English (US)
+            switch (einträge[i].name_id)
+            {
+                case 1: // Artname (Font Family)
+                    if (!schrift->artname) {
+                        schrift->artname = _strdup(einträge[i].string);
+                    }
+                    break;
+                case 2: // Stilname (Font Subfamily)
+                    if (!schrift->stilname) {
+                        schrift->stilname = _strdup(einträge[i].string);
+                    }
+                    break;
+            }
+        }
+
+        // Zurück zur ursprünglichen Position
+        fseek(datei, current_pos, SEEK_SET);
+    }
+
+    // Aufräumen
+    for (int i = 0; i < anzahl; i++)
+    {
+        GLYPHE_FRG(einträge[i].string);
+    }
+    GLYPHE_FRG(einträge);
+}
+
+// Verbesserte Implementierung für die Schalter-Allokation und -Verarbeitung
+uint8_t*
+otf_schalter_einlesen(FILE* datei, uint16_t punkte_gesamt, size_t* finale_anzahl)
+{
+    // Initial mehr Speicher reservieren für mögliche Wiederholungen
+    size_t aktuelle_kapazität = punkte_gesamt * 2;  // Start mit doppelter Größe
+    uint8_t* schalter = (uint8_t*)GLYPHE_RES(aktuelle_kapazität);
+    size_t aktuelle_position = 0;
+
+    for (uint16_t j = 0; j < punkte_gesamt && aktuelle_position < aktuelle_kapazität; j++)
+    {
+        schalter[aktuelle_position] = fgetc(datei);
+
+        if (schalter[aktuelle_position] & 0x08)
+        {
+            // Wiederholungsschalter
+            uint8_t wiederholungen = fgetc(datei);
+
+            // Prüfen ob mehr Speicher benötigt wird
+            size_t benötigter_speicher = aktuelle_position + wiederholungen + 2;
+            if (benötigter_speicher > aktuelle_kapazität)
+            {
+                aktuelle_kapazität = benötigter_speicher * 2;  // Verdoppeln für zukünftige Wiederholungen
+                uint8_t* neuer_speicher = (uint8_t*)GLYPHE_RES(aktuelle_kapazität);
+                memcpy(neuer_speicher, schalter, aktuelle_position + 1);
+                GLYPHE_FRG(schalter);
+                schalter = neuer_speicher;
+            }
+
+            // Wiederholungen kopieren
+            for (uint8_t k = 0; k < wiederholungen; k++)
+            {
+                schalter[++aktuelle_position] = schalter[aktuelle_position-1];
+            }
+        }
+        aktuelle_position++;
+    }
+
+    *finale_anzahl = aktuelle_position;
+    return schalter;
 }
 
 // Funktion zum Einlesen des Font Headers
@@ -199,12 +377,7 @@ otf_schrift_lesen(const char* dateiname)
 
     // name Tabelle für Font-Namen
     otf_tabelle_t* name_tabelle = otf_tabelle_finden(schrift, "name");
-    if (name_tabelle)
-    {
-        fseek(datei, name_tabelle->versatz, SEEK_SET);
-        // Hier würde die Verarbeitung der name Tabelle folgen
-        // Dies ist komplex und erfordert zusätzliche Strukturen für String-IDs
-    }
+    otf_name_tabelle_lesen(datei, schrift, name_tabelle);
 
     // maxp Tabelle für Glyph-Anzahl
     otf_tabelle_t* maxp_tabelle = otf_tabelle_finden(schrift, "maxp");
@@ -237,6 +410,7 @@ otf_schrift_lesen(const char* dateiname)
             glyphe->x_max = otf_s16_lesen(datei);
             glyphe->y_max = otf_s16_lesen(datei);
 
+            // Innerhalb der otf_schrift_lesen Funktion, im glyf_tabelle Block:
             if (glyphe->konturen_anzahl > 0)
             {
                 // Speicher für Konturen allozieren
@@ -253,18 +427,12 @@ otf_schrift_lesen(const char* dateiname)
                 uint16_t punkte_gesamt = endpunkte[glyphe->konturen_anzahl - 1] + 1;
 
                 // Schalter einlesen
-                uint8_t* schalter = (uint8_t *) GLYPHE_RES(punkte_gesamt);
-                for (uint16_t j = 0; j < punkte_gesamt; j++)
+                size_t schalter_anzahl = 0;
+                uint8_t* schalter = otf_schalter_einlesen(datei, punkte_gesamt, &schalter_anzahl);
+                if (!schalter)
                 {
-                    schalter[j] = fgetc(datei);
-                    if (schalter[j] & 0x08) // Wiederholungsschalter
-                    {
-                        uint8_t wiederholungen = fgetc(datei);
-                        for (uint8_t k = 0; k < wiederholungen; k++)
-                        {
-                            schalter[++j] = schalter[j-1];
-                        }
-                    }
+                    printf("schalter konnten nicht eingelesen werden");
+                    return NULL;
                 }
 
                 // X-Koordinaten einlesen
@@ -303,12 +471,16 @@ otf_schrift_lesen(const char* dateiname)
                 uint16_t punkt_index = 0;
                 for (int j = 0; j < glyphe->konturen_anzahl; j++)
                 {
-                    uint16_t punkte_in_kontur = (j == 0) ?
-                        endpunkte[0] + 1 :
-                        endpunkte[j] - endpunkte[j-1];
+                    uint16_t punkte_in_kontur = (j == 0)
+                        ? endpunkte[0] + 1
+                        : endpunkte[j] - endpunkte[j-1];
 
                     glyphe->konturen[j].punkte_anzahl = punkte_in_kontur;
-                    glyphe->konturen[j].punkte = (otf_glyphe_punkt_t *) GLYPHE_RES(sizeof(otf_glyphe_punkt_t) * punkte_in_kontur);
+                    glyphe->konturen[j].punkte = (otf_punkt_t *) GLYPHE_RES(sizeof(otf_punkt_t) * punkte_in_kontur);
+
+                    // INFO: punkte_in_kontur kann größer als punkte_gesamt sein und der zugriff auf
+                    //       die x- und y-koordinaten in der unteren schleife führt zu speicherzugriffsverletzung.
+                    printf("anzahl punkte: %d, anzahl x_koord: %d\n", punkte_in_kontur, punkte_gesamt);
 
                     for (uint16_t k = 0; k < punkte_in_kontur; k++)
                     {
@@ -319,11 +491,11 @@ otf_schrift_lesen(const char* dateiname)
                     }
                 }
 
-                // Temporäre Arrays freigeben
+                // Temporäre Felder erst NACH der vollständigen Verarbeitung freigeben
                 GLYPHE_FRG(endpunkte);
-                GLYPHE_FRG(schalter);
                 GLYPHE_FRG(x_koord);
                 GLYPHE_FRG(y_koord);
+                GLYPHE_FRG(schalter);
             }
         }
     }
